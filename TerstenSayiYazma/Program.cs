06d3e437d0de87ba195da6514f7996c74271a32b
Mustafa ALTUNTAŞ <2altuntas9@gmail.com>
2022-03-05 23:22:03 +0300

    Add project files.

---
+++ TerstenSayiYazma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TerstenSayiYazma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tekrarSayiAl = true;
+            int girilenSayi = 0;
+            while (tekrarSayiAl)
+            {
+                Console.Write("Pozitif bir tam sayı giriniz: ");
+                girilenSayi = int.Parse(Console.ReadLine());
+                if(girilenSayi < 0)
+                    tekrarSayiAl = true;
+                else
+                    tekrarSayiAl = false;
+            }
+
+            int tempSayi = girilenSayi;
+            string tempStr = "";
+            while(tempSayi % 10 >= 0 && tempSayi != 0)
+            {
+                tempStr = tempStr + (tempSayi % 10);
+                tempSayi = tempSayi / 10; 
+            }
+
+            Console.WriteLine("Girilen Sayı : "+girilenSayi);
+            Console.WriteLine("Sayının Tersi: "+int.Parse(tempStr));
+
+            Console.ReadLine();
+        }
+    }
+}
